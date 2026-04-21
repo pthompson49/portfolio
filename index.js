f7aee5e6cc12e7cc3605843fa27fef6b9ccdade9
pthompson49 <phillipthompson49@yahoo.co.uk>
2026-04-21 13:18:23 +0100

    content changes to add additional images

--- index.js
+++ index.js
@@ -8,3 +8,32 @@ window.addEventListener('scroll', () => {
     }
   });
 });
+
+const images = [
+  "./images/profile.png",
+  "./images/profile2.jpg",
+  "./images/profile3.jpg"
+];
+
+let currentIndex = 0;
+const heroImg = document.getElementById("hero-img");
+
+setInterval(() => {
+  heroImg.style.opacity = 0;
+
+  setTimeout(() => {
+    currentIndex = (currentIndex + 1) % images.length;
+    heroImg.src = images[currentIndex];
+    heroImg.style.opacity = 1;
+  }, 1800);
+}, 10000);
+
+const text = document.getElementById("hero-tagline");
+const letters = text.innerText.split("");
+
+text.innerHTML = letters
+  .map((letter, i) => {
+    if (letter === " ") return " ";
+    return `<span style="animation-delay:${2 + i * 0.04}s">${letter}</span>`;
+  })
+  .join("");
